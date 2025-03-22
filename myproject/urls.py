b4f4355f0a977ed9a8c48a23d19a39a1e9f616a2
Andrey AS <andrey5451@yandex.ru>
2025-03-23 01:00:31 +0300

    добавил админа1

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-
+from django.contrib import admin
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('admin/', admin.site.urls),
     # другие маршруты
 ]
