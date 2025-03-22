c64146ac07c2f7070f1f4857299547895c4763b2
Andrey AS <andrey5451@yandex.ru>
2025-03-23 01:08:34 +0300

    стили

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TELEGRAM_BOT_TOKEN = '7850159524:AAHAXQBEqsZ7VnnMAKqOy5TqELIGlYFhsiU'
 # Quick-start development settings - unsuitable for production
