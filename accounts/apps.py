5c97ad1b4bda31ddd1d595635b605ed381f188fb
Andrey AS <andrey5451@yandex.ru>
2025-03-23 11:53:38 +0300

    логирование1

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,9 +1,11 @@
-# accounts/apps.py
 from django.apps import AppConfig
+import logging
+
+logger = logging.getLogger(__name__)
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
         import accounts.signals
-        print("Сигналы из accounts.signals импортированы")
+        logger.info("Сигналы из accounts.signals импортированы")
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,19 +1,21 @@
-# accounts/signals.py
+import logging
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
+logger = logging.getLogger(__name__)
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        print("Сигнал: профиль создан для пользователя", instance)
+        logger.info("Сигнал: профиль создан для пользователя %s", instance)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     try:
         instance.profile.save()
-        print("Сигнал: профиль сохранён для пользователя", instance)
+        logger.info("Сигнал: профиль сохранён для пользователя %s", instance)
     except Exception as e:
-        print("Сигнал: Ошибка при сохранении профиля для пользователя", instance, ":", e)
+        logger.error("Ошибка при сохранении профиля для пользователя %s: %s", instance, e)
